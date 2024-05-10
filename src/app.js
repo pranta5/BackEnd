50d43f58be730048e98bb662452dcafb3ffc4cb2
pranta5 <prantadas01@gmail.com>
2024-05-10 18:02:18 +0530

    setup done

--- src/app.js
+++ src/app.js
@@ -11,7 +11,17 @@ app.use(cors({
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended: true,limit:"16kb"}))
-app.use.apply(express.static("public"))
+app.use(express.static("public"))
 app.use(cookieParser())
+
+
+//routes import 
+
+import userRouter from './routes/user.routes.js'
+
+// route declaration
+
+app.use("/api/v1/users",userRouter)
+
 
 export {app}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
-
+import {app} from './app.js'
 dotenv.config({
     path: '/.env'
 })
@@ -10,7 +10,7 @@ dotenv.config({
 connectDB()
 .then(()=>{
     app.listen(process.env.PORT || 7000,()=>{
-        console.log(1`server is running at port : ${process.env.PORT}`)
+        console.log(`server is running at port : ${process.env.PORT}`)
     })
 })
 .catch((err)=>{
