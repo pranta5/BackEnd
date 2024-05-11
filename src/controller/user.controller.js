46d903a0655a95f825844523fdb48091b4c046d5
pranta5 <prantadas01@gmail.com>
2024-05-12 02:34:27 +0530

    register controller

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -17,7 +17,6 @@ const registerUser = asyncHandler(async (req,res)=>{
 //return res
 
 const {username,email,fullName,password}=req.body
-console.log("email",email)
 
 if (
     [fullName,email,username,password].some((field)=>field?.trim() === "")
@@ -25,16 +24,22 @@ if (
     throw new ApiError(400,"all fields are required")
 }
 
-const existedUser =  User.findOne({
+const existedUser = await User.findOne({
     $or:[{username},{email}]
 })
 
 if (existedUser){
     throw new ApiError(409,"User with email or username is already exists")
 }
+// console.log(req.files)
 
 const avatarLocalPath = req.files?.avatar[0]?.path;
-const coverImageLocalPath = req.files?.coverImage[0]?.path
+// const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+let coverImageLocalPath;
+if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+    coverImageLocalPath = req.files.coverImage[0].path
+}
 
 if (!avatarLocalPath) {
     throw new ApiError(400,"Avatar file is required")
@@ -55,7 +60,7 @@ const user = await User.create({
     password,
     username: username.toLowerCase()
 })
-const createdUser = User.findById(user._id).select("-password -refreshToken")
+const createdUser =await User.findById(user._id).select("-password -refreshToken");
 if (!createdUser){
     throw new ApiError(500,"error while registering the user")
 }
